fce6af7fd818c36cbd70b6c5c90f0619e4feecc4
BigingiIan <bigingiian@gmail.com>
2025-09-04 13:48:55 +0300

    `Removed main.php and commented out password in ClassAutoLoad.php`

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader (created by composer, not included with PHPMailer)
-require 'vendor/autoload.php';
+require 'plugins/PHPMailer/vendor/autoload.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -23,21 +23,21 @@ try {
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('bigingiian@gmail.com', 'Mailer');
-    $mail->addAddress('ian.duncan@strathmore.edu', 'Joe User');     //Add a recipient
+    $mail->setFrom('no-reply@gmail.com', 'no-reply');
+    $mail->addAddress('bigingiian@gmail.com', 'New User');     //Add a recipient
     //$mail->addAddress('ellen@example.com');               //Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
     //$mail->addCC('cc@example.com');
     //$mail->addBCC('bcc@example.com');
 
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Welcome to the Sol Project';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = 'Welcome to the Solity Project';
+    $mail->Body    = 'Your email has been verified. Go back to login to your account';
     //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
